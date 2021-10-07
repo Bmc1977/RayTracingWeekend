d70a4ec9b12ebac2ab0a5ec1f1d3906fe9d05758
Bmc1977 <braxlamey@gmail.com>
2021-10-06 23:31:53 -0400

    Finished chapter 3. Code runs perfectly. Need to take a deeper look at the math/code implementation behind this chapter.

---
+++ Ray.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef RAYH
+#define RAYH
+
+#include "Vec3.h"
+
+class ray
+{
+public:
+	ray() {}
+	ray(const vec3& a, const vec3& b) { A = a; B = b; }
+	vec3 origin() const { return A;}
+	vec3 direction() const { return B; }
+	vec3 point_at_parameter(double t) const { return A + t * B;  }
+
+	vec3 A;
+	vec3 B;
+};
+
+#endif
